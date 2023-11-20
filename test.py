26bb51c5707819e310873b6b3e6a333558011bfe
aureliendaut <117839756+aureliendaut@users.noreply.github.com>
2023-11-20 11:37:03 +0100

    Modification stockage BF_support, test pour le cas 3D, à modif sur le cas 2D. A faire : test sur le cas 30x30x30 pour vérifier si les 2 matrices sont égales indépendamment du stockage + voir si le code tourne sur 100x100x100 (fonctionnel sur le cas 60x60x60)

---
+++ test.py
@@ -0,0 +1,16 @@
+
+import numpy as np 
+from local_Support_mM import *
+import scipy as sp
+ELEMENTS = np.loadtxt('ELEMENTS.dat')
+IND_mask = np.loadtxt('IND_mask.dat',dtype=int)
+IND_mask_tot = np.loadtxt('IND_mask_tot.dat',dtype=int)
+U1 = np.loadtxt('U1.dat')
+U2 = np.loadtxt('U2.dat')
+U3 = np.loadtxt('U3.dat')
+local_support, BF_support, IND_mask_active = local_support_fun(ELEMENTS, IND_mask, IND_mask_tot, U1, U2, U3, 'standard')
+BF_support2 = BF_support.toarray()
+print(BF_support2)
+
+
+
